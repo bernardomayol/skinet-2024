82bd64a669b48a949f680fbdf7eba596ac35b5f3
Bernardo Mayol <bernardomayol@yahoo.com>
2025-10-13 14:02:16 -0700

    End of session 4

---
+++ Core/Interfaces/IGenericRepository.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T?> GetEntityWithSpec(ISpecification<T> spec);
+        Task<TResult?> GetEntityWithSpec<TResult>(ISpecification<T, TResult> spec);
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+        Task<IReadOnlyList<TResult>> ListAsync<TResult>(ISpecification<T, TResult> spec);
+
+        void Add(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+        Task<bool> SaveAllAsync();
+        bool Exists(int id);
+        //Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+        //Task<T?> GetEntityWithSpec(ISpecification<T> spec);
+        //Task<int> CountAsync(ISpecification<T> spec);
+    }
+}
