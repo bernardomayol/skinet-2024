5fd8c35bd87981d032da823e40a0a342e0e52e99
Bernardo Mayol <bernardomayol@yahoo.com>
2025-10-16 10:45:34 -0700

    End of section 6

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,38 @@
+﻿using API.DTOs;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("unauthorized")]
+        public ActionResult GetUnauthorized()
+        {
+            return Unauthorized(); // 401
+        }
+        [HttpGet("notfound")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound(); // 404
+
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest("This is a bad request"); // 400           
+        }
+        [HttpGet("internalerror")]
+        public ActionResult GetInternalError()
+        {
+            throw new Exception("This is an internal error"); // 500
+        }
+        [HttpPost("validationerror")]
+        public ActionResult GetValidationError(CreateProductDto product)
+        {
+            return Ok();
+        } 
+
+    }
+}
---
+++ API/Errors/ApiErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Errors
+{
+    public class ApiErrorResponse(int statuscode, string message, string? details)
+    {
+        public int StatusCode { get; set; } = statuscode;
+        public string Message { get; set; } = message;
+        public string? Details { get; set; } = details;
+    }
+}
